a5570819746a777c29dff7c6d19020b16cf3e793
Christian Rudder <ringthebellpepper@gmail.com>
2023-01-16 21:34:37 -0500

    on off switch works

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 const DEFAULT = {
+  onSwitch: true,
   reqBlur: "50%",
   greyscale: "100%",
   brightness: "0%",
@@ -9,41 +10,23 @@ const DEFAULT = {
   lowercase: true,
   clickBaitHidden: true,
 } 
-console.log("hello")
-// chrome.tabs.onUpdated.addListener( async (tab) => {
-//   console.log("bruhbruh");
-//   console.log(await tab.url)
-//   console.log(tab.url && tab.url.includes("youtube.com"), tab, tab.url.includes("youtube.com"))
-//   if(tab.url && tab.url.includes("youtube.com")) {
-//     let url = new URL(tab.url);
-//     let pathname = url.pathname.split("/")[1];
-//     console.log("lol", pathname, !pathname)
-//     if (!pathname) {
-//       await chrome.scripting.insertCSS({
-//         files: ["./styles/focus-home.css"],
-//         target: { tabId: tab.id },
-//       });
-//     }
-//     // console.log("lol")
-//     // chrome.tabs.sendMessage(tabId, {
-//     //   type: "NEW",
-//     //   pathname,
-//     // });
-    
-//   }
-// });
+
+console.log("yo")
 
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+
   if (changeInfo.status === "complete") {
+
     chrome.tabs.query({active: true, currentWindow: true}, async function(tabs) {
         let tab = tabs[0];
+
         if (tab.url.includes("youtube.com")) {
+
           let url = new URL(tab.url);
           let pathname = url.pathname.split("/")[1];
-          console.log("pathsss:",pathname, "tab:", tab, "jomba",pathname === "")
+          
           if (pathname === "") {
-            console.log("path:",pathname)            
-
+            
             await chrome.scripting.insertCSS({
               files: ["focus-home.css"],
               target: { tabId: tab.id },
@@ -69,4 +52,4 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
 
   
   
-// });
+
